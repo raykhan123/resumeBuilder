98a480e8202a5e0324569a5d159be9f541387578
raykhan123 <raykhan.rk123@gmail.com>
2022-09-14 17:07:14 +0530

    userModel

---
+++ src/models/userModel.js
@@ -0,0 +1,48 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+
+    firstName:{
+        type:String,
+        require:true,
+        unique:true,
+        trim:true,
+        lowercase: true
+    },
+    lastName:{
+        type:String,
+        require:true,
+        trim:true
+    },
+    email:{
+        type:Number,
+        require:true,
+        trim:true
+    },
+    mobileNo:{
+        type:Number,
+        require:true,
+        trim:true
+    },
+   
+    pasword:{
+        type:String,
+        require:true,
+        trim:true
+    },
+    confirmPasword:{
+        type:String,
+        require:true,
+        trim:true
+    },
+   
+    isDeleted:{
+        type:Boolean,
+        default:false,
+        trim:true
+    }
+
+
+}, { timestamps: true });
+
+module.exports = mongoose.model('User', userSchema)
