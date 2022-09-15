1a8f25cc6eef21ad4dc41f90945cd8b59eec226b
Adhish Basak <adhishbasak98@gmail.com>
2022-09-15 19:56:49 +0530

    Update User Created and BoilerPlate of Login User Added

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -15,7 +15,7 @@ const createUser = async (req, res) => {
 
         const data = await userModel.create(userData);
 
-        res.status(201).send({ status: true, data });
+        return res.status(201).send({ status: true, data });
 
     }
     catch (err) {
@@ -23,4 +23,29 @@ const createUser = async (req, res) => {
     }
 }
 
-module.exports = { createUser };
+const updateUser = async (req, res) => {
+
+    try{
+        let update = req.body
+        if(Object.keys(update) == 0 ) return res.status(400).send({status : false, message: "Need One field to be updated !"})
+        let pasword = CryptoJS.AES.encrypt(userData.pasword, 'fantastic4-@$%').toString();
+        update.pasword = pasword;
+        const data = await userModel.findOneAndUpdate({_id : id}, update, {new : true})
+        return res.status(200).send({ status: true, data });
+    }
+    catch (err){
+        return res.status(500).send({ status: false, message: err.message });
+    }
+   
+}
+
+const login = async (req, res) => {
+    try{
+        
+    }
+    catch(err){
+        return res.status(500).send({ status: false, message: err.message });
+    }
+}
+
+module.exports = { createUser, updateUser, login };
--- src/routers/route.js
+++ src/routers/route.js
@@ -1,10 +1,15 @@
 const express = require("express");
 const router = express.Router();
-const {createUser} = require('../controllers/userController');
+const {createUser, updateUser,login} = require('../controllers/userController');
 
 
 //////// APIs 
 router.post('/createUser', createUser)
-
+router.put('/updateUset', updateUser)
+router.post('/login', login)
 
 module.exports = router;
+
+//2.login
+//3.get api
+//4.put api
